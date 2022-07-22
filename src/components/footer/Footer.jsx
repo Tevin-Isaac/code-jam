6c45b73df2b32f3f61d77528e78438b46511b82a
Tevin-creator <isaactevin67@gmail..com>
2022-07-22 19:44:04 +0300

    updated default app

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './footer.css'
+
+import { AiOutlineInstagram,AiOutlineTwitter, } from "react-icons/ai";
+import { RiDiscordFill } from "react-icons/ri";
+import { FaTelegramPlane } from "react-icons/fa";
+const Footer = () => {
+  return (
+    <div className='footer section__padding'>
+      <div className="footer-links">
+        <div className="footer-links_logo">
+        <div>
+        
+          <p>CeloHodler</p>
+        </div>
+
+   </div>
+   </div>
+   </div>
+  )
+}
+
+export default Footer
